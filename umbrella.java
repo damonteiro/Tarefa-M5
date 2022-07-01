e3b1546f00b24b3a64677813f4fb83079708e648
Diego Monteiro <damonteiro89@gmail.com>
2022-07-01 19:08:55 -0300

    Adicionando tarefa-m4

---
+++ umbrella.java
@@ -0,0 +1,9 @@
+public class umbrella {
+
+        public static void main(String[] args) {
+           String ambiente = args[0];
+            if (ambiente.equals("DEV)"));
+             System.out.print("Primeiro passo para minha transição de carreira S2");
+
+         }
+    }
